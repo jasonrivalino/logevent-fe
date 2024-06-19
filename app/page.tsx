62e438befa669785a9eb35877aed421c39d44e44
jasonrivalino <13521008@std.stei.itb.ac.id>
2024-06-19 22:42:29 +0800

    feat: update and test configuration

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
-          width={180}
+          width={500}
           height={37}
           priority
         />
