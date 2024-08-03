062a58fde562b93e059b12a9dac7816538205b4b
jasonrivalino <13521008@std.stei.itb.ac.id>
2024-08-03 21:34:22 +0800

    feat: add product vendor responsive

--- app/admin/manage-vendor/product/edit/page.tsx
+++ app/admin/manage-vendor/product/edit/page.tsx
@@ -5,14 +5,38 @@ import { useRouter, usePathname } from 'next/navigation';
 import { CommandLeft } from '../../../commandLeft';
 
 export default function AdminEventPackage() {
-    return (
-      <div className="overflow-hidden">
-        <div className="min-h-screen flex flex-col px-10 pt-10 mt-16">
-            <Navbar />
-            <div className="flex flex-col md:flex-row flex-grow">
-                <CommandLeft />
+  const router = useRouter();
+  const handleBackClick = () => {
+      router.push('/admin/manage-vendor/product');
+  }
+
+  return (
+    <div>
+      <Navbar />
+          <div className="min-h-screen flex flex-col px-6 mt-24 mb-14">
+              <div className="flex flex-col md:flex-row flex-grow">
+                <div className="md:hidden flex justify-center items-center">
+                      {/* Back button with SVG arrow */}
+                      <button 
+                        onClick={handleBackClick} 
+                        className="absolute top-20 left-4 p-2 rounded-full bg-white text-black shadow-lg flex items-center justify-center w-10 h-10 md:w-12 md:h-12 hover:bg-gray-100"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          className="w-6 h-6 text-gray-700"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                        </svg>
+                      </button>
+                </div>
+                <div className="hidden md:block">
+                      <CommandLeft />
+                </div>
                 <div className="flex-grow ml-0 md:ml-7 pt-[0.15rem]">
-                    <AddVendorProduct />
+                    <EditVendorProduct />
                 </div>
             </div>
         </div>
@@ -20,7 +44,7 @@ export default function AdminEventPackage() {
     );
 }
 
-function AddVendorProduct() {
+function EditVendorProduct() {
     const router = useRouter(); // Initialize useRouter for navigation
     const [selectedCategory, setSelectedCategory] = useState('');
     const [selectedPriceUnit, setSelectedPriceUnit] = useState('');
@@ -36,17 +60,13 @@ function AddVendorProduct() {
         setSelectedPriceUnit('');
       }
     };
-  
-    const handlePriceUnitChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-      setSelectedPriceUnit(event.target.value);
-    };
 
     const [photos, setPhotos] = useState<File[]>([
-        new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
-        new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
-        new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
-        new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
-        new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
+      new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
+      new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
+      new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
+      new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
+      new File([''], 'https://via.placeholder.com/150', { type: 'image/png' }),
     ]);
 
     const handlePhotoUpload: React.ChangeEventHandler<HTMLInputElement> = (event) => {
@@ -66,7 +86,7 @@ function AddVendorProduct() {
     return (
       <div className="px-6 pt-4 pb-6 bg-white rounded-xl shadow-md">
         {/* Text in center */}
-        <h1 className="text-3xl font-bold mb-3 text-pink-900 font-sofia text-center">Edit Produk A</h1>
+        <h1 className="text-2xl md:text-3xl font-bold md:mb-3 text-pink-900 font-sofia text-center mt-5 md:mt-0">Edit Produk A</h1>
         {/* Breadcrumb Navigation */}
         <div className="hidden md:flex items-center mb-4">
           <div className="flex items-center">
@@ -74,138 +94,101 @@ function AddVendorProduct() {
             <span className="mx-2 text-gray-600 font-sofia font-semibold"> {'>'} </span>
             <a onClick={() => router.push('/admin/manage-vendor/product')} className="text-pink-600 font-semibold font-sofia cursor-pointer">Kelola Produk</a>
             <span className="mx-2 text-gray-600 font-sofia font-semibold"> {'>'} </span>
-            <p className="text-gray-600 font-sofia font-semibold">Edit Produk</p>
+            <p className="text-gray-600 font-sofia font-semibold">Tambah Produk</p>
           </div>
         </div>
         {/* Form */}
-        <form className="grid grid-cols-1 gap-4 font-sofia text-black">
-          <div className="flex space-x-6">
-            <div className="w-2/5">
-              <label className="block text-gray-700 font-sofia">Nama Produk *</label>
-              <p className="text-gray-500 text-sm font-sofia mb-2">Nama maksimal 40 karakter dengan memasukkan nama barang</p>
-              <input className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" type="text" placeholder="Contoh: Gedung Sabuga ITB" maxLength={40} />
+        <form className="grid grid-cols-1 gap-4 font-sofia text-black mt-6 md:mt-0">
+          <div className="flex flex-col md:flex-row md:space-x-6">
+            <div className="w-full md:w-2/5 mb-3 md:mb-0">
+              <label className="block text-gray-700 font-sofia text-sm md:text-base">Nama Produk *</label>
+              <p className="text-gray-500 text-xs md:text-sm font-sofia mb-1 md:mb-2">Nama maksimal 40 karakter dengan memasukkan nama barang</p>
+              <input className="w-full px-3 md:px-4 py-1 md:py-2 text-sm md:text-base border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" type="text" placeholder="Contoh: Gedung Sabuga ITB" maxLength={40} />
             </div>
-            <div className="w-[30%]">
-              <label className="block text-gray-700 font-sofia">Spesifikasi Produk *</label>
-              <p className="text-gray-500 text-sm font-sofia mb-2">Spesifikasi produk yang dijual oleh vendor</p>
-              <input className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" type="text" placeholder="Multifunction Hall" />
+            <div className="w-full md:w-[30%] mb-3 md:mb-0">
+              <label className="block text-gray-700 font-sofia text-sm md:text-base">Spesifikasi Produk *</label>
+              <p className="text-gray-500 text-xs md:text-sm font-sofia mb-1 md:mb-2">Spesifikasi produk yang dijual oleh vendor</p>
+              <input className="w-full px-3 md:px-4 py-1 md:py-2 text-sm md:text-base border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" type="text" placeholder="Multifunction Hall" />
             </div>
-            <div className="w-[30%]">
-              <label className="block text-gray-700 font-sofia">Kapasitas Produk</label>
-              <p className="text-gray-500 text-sm font-sofia mb-2">Kosongkan jika produk tidak memiliki kapasitas</p>
-              <input className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" type="text" placeholder="Masukkan Kapasitas" />
-            </div>
-          </div>
-          <div className="flex space-x-6">
-            <div className="w-full">
-              <label className="block text-gray-700 font-sofia">Deskripsi Produk *</label>
-              <p className="text-gray-500 text-sm font-sofia mb-2">Pastikan deskripsi produk memuat penjelasan detail terkait produkmu agar pembeli mudah mengerti dan menemukan produkmu</p>
-              <textarea rows={3} className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" placeholder="Gedung Sabuga ITB adalah gedung Sasana Budaya Ganesha"></textarea>
+            <div className="w-full md:w-[30%]">
+              <label className="block text-gray-700 font-sofia text-sm md:text-base">Kapasitas Produk</label>
+              <p className="text-gray-500 text-xs md:text-sm font-sofia mb-1 md:mb-2">Kosongkan jika produk tidak memiliki kapasitas</p>
+              <input className="w-full px-3 md:px-4 py-1 md:py-2 text-sm md:text-base border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" type="text" placeholder="Masukkan Kapasitas" />
             </div>
           </div>
-          <div className="flex space-x-6">
-            <div className="w-1/5">
-              <label className="block text-gray-700 font-sofia mb-2">Kategori Produk *</label>
-              <select
-                className="w-full px-4 py-[0.65rem] border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600 bg-white"
-                value={selectedCategory}
-                onChange={handleCategoryChange}
-              >
-                <option value="">Pilih Kategori</option>
-                <option value="katering">Katering</option>
-                <option value="sound_system">Sound System</option>
-                <option value="gedung">Gedung</option>
-                <option value="tambah_kategori">+ Tambah Kategori</option>
-              </select>
+          <div className="md:flex space-x-6">
+            <div className="w-full -mt-1 md:mt-4">
+              <label className="block text-gray-700 font-sofia text-sm md:text-base">Deskripsi Paket *</label>
+              <p className="text-gray-500 text-xs md:text-sm font-sofia mb-1 md:mb-2">Pastikan deskripsi paket memuat penjelasan detail terkait paketmu agar pembeli mudah mengerti dan menemukan paketmu</p>
+              <textarea rows={3} className="w-full px-2 md:px-4 py-1 md:py-2 text-sm md:text-base border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600" placeholder="Gedung Sabuga ITB adalah gedung Sasana Budaya Ganesha"></textarea>
             </div>
-            <div className="w-[30%]">
-              <label className="block text-gray-700 font-sofia mb-2">Harga Produk *</label>
-              <div className="flex">
-                <span className="flex items-center px-3 text-gray-500 border border-r-0 rounded-l-lg border-gray-300">Rp</span>
-                <input className="w-full px-4 py-2 border rounded-r-lg focus:outline-none focus:ring-2 focus:ring-pink-600" type="text" placeholder="Masukkan Harga" />
+          </div>
+          <div className="flex flex-col md:flex-row md:space-x-6">
+            <div className="flex flex-col md:flex-row -mt-2 md:mt-0">
+              <div className="w-full mb-2 md:mb-0">
+                <label className="block text-gray-700 font-sofia mb-1 md:mb-2 text-sm md:text-base">Kategori Paket *</label>
+                <select
+                  className="w-full md:w-11/12 px-2 md:px-4 py-1 md:py-[0.65rem] border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-600 bg-white text-sm md:text-base"
+                  value={selectedCategory}
+                  onChange={handleCategoryChange}
+                >
+                  <option value="">Pilih Kategori</option>
+                  <option value="katering">Katering</option>
+                  <option value="sound_system">Sound System</option>
+                  <option value="gedung">Gedung</option>
+                  <option value="tambah_kategori">+ Tambah Kategori</option>
+                </select>
               </div>
-              <div className="flex mt-1 space-x-4">
-                <label className="flex items-center text-gray-700 font-sofia">
-                  <input
-                    type="radio"
-                    name="harga"
-                    value="/jam"
-                    className="mr-2"
-                    checked={selectedPriceUnit === '/jam'}
-                    onChange={handlePriceUnitChange}
-                    disabled={selectedCategory === 'katering'}
-                  />
-                  / Jam
-                </label>
-                <label className="flex items-center text-gray-700 font-sofia">
-                  <input
-                    type="radio"
-                    name="harga"
-                    value="/hari"
-                    className="mr-2"
-                    checked={selectedPriceUnit === '/hari'}
-                    onChange={handlePriceUnitChange}
-                    disabled={selectedCategory === 'katering'}
-                  />
-                  / Hari
-                </label>
-                <label className="flex items-center text-gray-700 font-sofia">
-                  <input
-                    type="radio"
-                    name="harga"
-                    value="/pcs"
-                    className="mr-2"
-                    checked={selectedPriceUnit === '/pcs'}
-                    onChange={handlePriceUnitChange}
-                    disabled={selectedCategory !== 'katering'}
-                  />
-                  / Pcs
-                </label>
+              <div className="w-full md:w-[21rem]">
+                <label className="block text-gray-700 font-sofia mb-1 md:mb-2 text-sm md:text-base">Harga Paket *</label>
+                <div className="flex">
+                  <span className="flex items-center px-3 text-gray-500 border border-r-0 rounded-l-lg border-gray-300">Rp</span>
+                  <input className="w-full px-4 py-1 md:py-2 border rounded-r-lg focus:outline-none focus:ring-2 focus:ring-pink-600 text-sm md:text-base" type="text" placeholder="Masukkan Harga" />
+                </div>
               </div>
             </div>
-            <div className="w-1/2">
-                <div className="flex flex-row">
-                    <div className="flex flex-col w-1/2">
-                        <label className="block text-gray-700 font-sofia">Foto Produk *</label>
-                        <p className="text-gray-500 text-sm font-sofia mb-2">Masukkan 5 foto produk</p>
+            <div className="w-full md:w-1/2">
+              <div className="flex flex-row mt-4">
+                <div className="flex flex-col w-1/2">
+                  <label className="block text-gray-700 font-sofia text-sm md:text-base -mt-2 md:mt-0">Foto Paket (masukkan 5 foto) *</label>
+                </div>
+                <label className="border bg-pink-600 px-2 md:py-1 -mt-1 mb-3 rounded-lg cursor-pointer flex justify-center items-center ml-auto">
+                  <span className="text-white text-sm md:text-base">Upload Image</span>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    multiple
+                    className="hidden"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => handlePhotoUpload(event)}
+                  />
+                </label>
+              </div>
+              <div className="border border-dashed border-gray-400 rounded-lg flex justify-center items-center flex-wrap min-h-[50px] mt-2 md:mt-0">
+                {photos.length === 0 ? (
+                  <span className="text-gray-500">Upload a photo</span>
+                ) : (
+                  photos.map((photo, index) => (
+                    <div key={index} className="relative m-2">
+                      <img src={URL.createObjectURL(photo)} alt={`upload-${index}`} className="w-10 h-10 object-cover rounded" />
+                      <button
+                        type="button"
+                        onClick={() => handleRemovePhoto(index)}
+                        className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 text-xs flex justify-center items-center"
+                      >
+                        &times;
+                      </button>
                     </div>
-                    <label className="border bg-pink-600 px-2 py-1 mb-3 rounded-lg cursor-pointer flex justify-center items-center ml-auto">
-                        <span className="text-white">Upload Image</span>
-                        <input
-                            type="file"
-                            accept="image/*"
-                            multiple
-                            className="hidden"
-                            onChange={handlePhotoUpload}
-                        />
-                    </label>
-                </div>
-                <div className="border border-dashed border-gray-400 rounded-lg flex justify-center items-center flex-wrap min-h-[50px]">
-                    {photos.length === 0 ? (
-                        <span className="text-gray-500">Upload a photo</span>
-                    ) : (
-                        photos.map((photo, index) => (
-                            <div key={index} className="relative m-2">
-                                <img src={URL.createObjectURL(photo)} alt={`upload-${index}`} className="w-10 h-10 object-cover rounded" />
-                                <button
-                                    type="button"
-                                    onClick={() => handleRemovePhoto(index)}
-                                    className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 text-xs flex justify-center items-center"
-                                >
-                                    &times;
-                                </button>
-                            </div>
-                        ))
-                    )}
-                </div>
+                  ))
+                )}
+              </div>
             </div>
-            </div>
-            <div className="w-full">
-                <button type="submit" className="w-full py-2 mt-2 bg-pink-600 text-white font-semibold rounded-lg hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-600" onClick={() => router.push('/admin/manage-vendor/product')}>
-                    Edit Produk
-                </button>
-            </div>
+          </div>
+          <div className="w-full">
+            <button type="submit" className="w-full py-1 md:py-2 mt-2 bg-pink-600 text-white font-semibold rounded-lg hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-600" onClick={() => router.push('/admin/manage-event-package')}>
+              Edit Produk
+            </button>
+          </div>
         </form>
       </div>
     );
-}
+  }
--- app/admin/manage-vendor/product/page.tsx
+++ app/admin/manage-vendor/product/page.tsx
@@ -137,15 +137,39 @@ const dummyVendors: Vendor[] = [
   ];
 
 export default function AdminEventPackage() {
+    const router = useRouter();
+    const handleBackClick = () => {
+        router.push('/admin/manage-vendor');
+    }
+
     return (
       <div>
-        <div className="min-h-screen flex flex-col p-10 mt-16">
-            <Navbar />
-            <div className="flex flex-col md:flex-row flex-grow">
-                <CommandLeft />
-                <div className="flex-grow ml-0 md:ml-7 py-[0.15rem]">
-                    <ManageVendorProduct vendors={dummyVendors} />
-                </div>
+        <Navbar />
+            <div className="min-h-screen flex flex-col px-6 mt-24">
+                <div className="flex flex-col md:flex-row flex-grow">
+                  <div className="md:hidden flex justify-center items-center">
+                        {/* Back button with SVG arrow */}
+                        <button 
+                          onClick={handleBackClick} 
+                          className="absolute top-20 left-4 p-2 rounded-full bg-white text-black shadow-lg flex items-center justify-center w-10 h-10 md:w-12 md:h-12 hover:bg-gray-100"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            className="w-6 h-6 text-gray-700"
+                          >
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                          </svg>
+                        </button>
+                  </div>
+                  <div className="hidden md:block">
+                        <CommandLeft />
+                  </div>
+                  <div className="flex-grow ml-0 md:ml-7 py-[0.15rem]">
+                      <ManageVendorProduct vendors={dummyVendors} />
+                  </div>
             </div>
         </div>
         <ContactBox />
@@ -188,17 +212,18 @@ function ManageVendorProduct({ vendors }: { vendors: Vendor[] }) {
   const totalPages = Math.ceil(vendorList.length / itemsPerPage);
 
   return (
-    <div className="px-8 pt-6 pb-10 bg-white rounded-xl shadow-md">
-      <h1 className="text-3xl font-bold mb-6 text-pink-900 font-sofia">Welcome Admin LogEvent!</h1>
+    <div className="px-4 md:px-8 pt-6 pb-10 bg-white rounded-xl shadow-md">
+      <h1 className="md:hidden text-2xl md:text-3xl font-bold mb-6 md:mb-3 text-pink-900 font-sofia text-center">Product List</h1>
+      <h1 className="hidden md:block text-3xl font-bold mb-6 text-pink-900 font-sofia">Welcome Admin LogEvent!</h1>
       {/* Breadcrumb Navigation */}
-      <div className="hidden md:flex items-center mb-6">
-        <div className="flex items-center">
+      <div className="md:flex items-center mb-6">
+        <div className="hidden md:flex items-center">
           <a onClick={() => router.push('/admin/manage-vendor')} className="text-pink-600 font-semibold font-sofia cursor-pointer">Kelola Vendor</a>
           <span className="mx-2 text-gray-600 font-sofia font-semibold"> {'>'} </span>
           <span className="text-gray-600 font-sofia font-semibold">Kelola Produk</span>
         </div>
         {/* Search Bar */}
-        <div className="flex items-center ml-auto">
+        <div className="flex items-center md:ml-auto">
           <div className="relative">
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <svg className="w-3 md:w-4 h-3 md:h-4 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
@@ -208,18 +233,18 @@ function ManageVendorProduct({ vendors }: { vendors: Vendor[] }) {
             <input
               type="text"
               placeholder="Cari kebutuhan vendormu"
-              className="w-full text-sm md:text-base p-1 md:p-2 pl-9 md:pl-12 border rounded bg-white text-black font-sofia"
+              className="w-[17.5rem] md:w-full text-sm md:text-base p-1 md:p-2 pl-9 md:pl-12 border rounded bg-white text-black font-sofia"
             />
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-between bg-white rounded-xl p-3 mb-6 shadow-md border-pink-600 border-2 font-sofia">
-        <div className="flex items-center ml-5">
+      <div className="flex flex-col md:flex-row md:items-center justify-between bg-white rounded-xl p-1 md:p-3 mb-6 -mt-2 mb:mt-0 shadow-md border-pink-600 border-2 font-sofia">
+        <div className="flex md:items-center ml-3 md:ml-5">
           <span className="text-lg md:text-xl text-pink-900 font-bold mr-4">Vendor B</span>
         </div>
-        <div className="flex items-center">
-          <span className="text-sm md:text-base text-gray-700 px-5">Jumlah Produk: 17</span>
-          <button className="bg-pink-500 text-white px-3 py-1 rounded-full font-bold text-xs md:text-base mr-4" onClick={() => router.push('/admin/manage-vendor/product/add')}>
+        <div className="flex md:items-center">
+          <span className="text-xs md:text-base text-gray-700 px-3 md:px-5 mr-12 md:mr-0">Jumlah Produk: 17</span>
+          <button className="bg-pink-500 text-white px-1 md:px-3 py-1 rounded-full font-bold text-xs md:text-base -mt-1 md:mt-0 mb-1 md:mb-0 md:mr-4" onClick={() => router.push('/admin/manage-vendor/product/add')}>
             + Tambah Produk
           </button>
         </div>
@@ -258,13 +283,13 @@ function ManageVendorProduct({ vendors }: { vendors: Vendor[] }) {
                 >
                   Lihat Detail
                 </button>
-                <div className="flex space-x-2">
+                <div className="flex space-x-1 md:space-x-2">
                   <FaEdit
-                    className="text-pink-500 cursor-pointer hover:text-pink-700"
+                    className="text-pink-500 cursor-pointer hover:text-pink-700 w-3 md:w-4 h-3 md:h-4"
                     onClick={() => router.push('/admin/manage-vendor/product/edit')}
                   />
                   <FaTrashAlt
-                    className="text-pink-500 cursor-pointer hover:text-pink-700"
+                    className="text-pink-500 cursor-pointer hover:text-pink-700 w-3 md:w-4 h-3 md:h-4"
                     onClick={() => handleDeleteClick(vendor)}
                   />
                 </div>
